f6bbe126d18c06b6a69a9e9196283e0580cb41cf
Jaqueline Broccolo <j.silvabroccolo@gmail.com>
2024-03-12 22:27:29 -0300

    feat: add parte 'frontend'

---
+++ web/src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import styled from "styled-components"
+import IconesHeader from "../IconesHeader"
+import Logo from "../Logo"
+import OpcoesHeader from "../OpcoesHeader"
+
+const HeaderContainer = styled.header`
+    background-color: #FFF;
+    display: flex;
+`
+
+function Header() {
+    return (
+        <HeaderContainer>
+            <Logo />
+            <OpcoesHeader />
+            <IconesHeader />
+        </HeaderContainer>
+    )
+}
+
+export default Header
