d72d6d5a4267070be788efbc3a40e1534d44b91d
Jaqueline Broccolo <j.silvabroccolo@gmail.com>
2024-03-19 21:19:14 -0300

    Lidando com erros

--- server/controladores/livro.js
+++ server/controladores/livro.js
@@ -14,8 +14,15 @@ function getLivros(req, res) {
 function getLivro(req, res) {
     try {
         const id = req.params.id
-        const livro = getLivroPorID(id)
-        res.send(livro)
+
+        if (id && Number(id)) {
+            const livro = getLivroPorID(id)
+            res.send(livro)
+
+        } else {
+            res.status(422)
+            res.send('ID INVÁLIDO')
+        }
 
     } catch (error) {
         res.status(500)
@@ -26,9 +33,16 @@ function getLivro(req, res) {
 function postLivro(req, res) {
     try {
         const livroNovo = req.body
-        insereLivro(livroNovo)
-        res.status(201)
-        res.send("Livro inserido com sucesso!")
+
+        if (req.body.nome) {
+            insereLivro(livroNovo)
+            res.status(201)
+            res.send("Livro inserido com sucesso!")
+
+        } else {
+            res.status(422)
+            res.send('O CAMPO "NOME" É OBRIGATÓRIO')
+        }
 
     } catch (error) {
         res.status(500)
@@ -39,10 +53,16 @@ function postLivro(req, res) {
 function patchLivro(req, res) {
     try {
         const id = req.params.id
-        const body = req.body
 
-        modificaLivro(body, id)
-        res.send(`Item ${id} modificado com sucesso!`)
+        if (id && Number(id)) {
+            const body = req.body
+            modificaLivro(body, id)
+            res.send(`Item ${id} modificado com sucesso!`)
+
+        } else {
+            res.status(422)
+            res.send('ID INVÁLIDO')
+        }
 
     } catch (error) {
         res.status(500)
@@ -54,8 +74,14 @@ function deleteLivro(req, res) {
     try {
         const id = req.params.id
 
-        apagaLivroPorID(id)
-        res.send(`Livro ${id} apagado com sucesso!`)
+        if (id && Number(id)) {
+            apagaLivroPorID(id)
+            res.send(`Livro ${id} apagado com sucesso!`)
+
+        } else {
+            res.status(422)
+            res.send('ID INVÁLIDO')
+        }
 
     } catch (error) {
         res.status(500)
