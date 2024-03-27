e301beca36acc932de05757aaf41de39f4f5b6a9
Jaqueline Broccolo <j.silvabroccolo@gmail.com>
2024-03-26 22:25:40 -0300

    style: rem. `width: 100vw` em 'Home'

--- web/src/rotas/Home.tsx
+++ web/src/rotas/Home.tsx
@@ -3,7 +3,6 @@ import Pesquisa from '../components/Pesquisa';
 import UltimosLancamentos from '../components/UltimosLancamentos';
 
 const AppContainer = styled.div`
-  width: 100vw;
   height: 100vh;
   background-image: linear-gradient(90deg, #002F52 35%, #326589);
 `
