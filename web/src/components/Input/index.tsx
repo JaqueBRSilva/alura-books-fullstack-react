09e2aab5531c47f356d75fd981bcbf0cc37bc6c0
Jaqueline Broccolo <j.silvabroccolo@gmail.com>
2024-03-29 16:22:49 -0300

    style: alterações na exibição e medidas do 'input'

--- web/src/components/Input/index.tsx
+++ web/src/components/Input/index.tsx
@@ -1,15 +1,15 @@
 import styled from 'styled-components'
 
 const Input = styled.input`
-    order: 1px solid #FFF;
     background: transparent;
     border: 1px solid #FFF;
-    padding: 20px 140px;
+    padding: 20px 5rem;
     border-radius: 50px;
-    width: 200px;
-    color: #FFF;
+    width: 20rem;
+    text-align: center;
     font-size: 16px;
-    margin-bottom: 10px;
+    color: #FFF;
+    margin: 10px 0;
 
     &::placeholder {
         color: #FFF;
